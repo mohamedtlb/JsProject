d88814eac65cc1cb1ce83fc2056ccd0190d81edd
mohamedtlb <mohamed231995@hotmail.fr>
2021-04-14 00:40:11 +0200

    premier commit

---
+++ questions.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        title: "Question1",
+        choices: ["proposition1", "proposition", "proposition", "proposition"],
+        answer: "proposition1"
+    },
+    {
+        title: "Question2",
+        choices: ["proposition", "proposition", "proposition", "proposition"],
+        answer: "rep"
+    },
+    ///etc.
+    {
+        title: "DQuestion3",
+        choices: ["proposition", "proposition", "proposition", "proposition"],
+        answer: "rep"
+    },
+    {
+        title: "Question4",
+        choices: ["proposition", "proposition", "proposition", "proposition"],
+        answer: "rep"
+    },
+    {
+        title: "Question5",
+        choices: ["proposition", "proposition", "proposition", "proposition"],
+        answer: "rep"
+    },
+    {
+        title: "Question6",
+        choices: ["proposition", "tproposition", "proposition", "proposition"],
+        answer: "rep"
+    }
+];
