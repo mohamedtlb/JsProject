1c74f636890f9666783fbcc833514cac9bd613fb
Lucas Lebret <lucaslebret.scolaire@gmail.com>
2021-05-06 03:12:36 +0200

    Reconnecting when connection is lost, test-3 connection pool

--- App.js
+++ App.js
@@ -19,33 +19,41 @@ app.listen(process.env.PORT);
 console.log('Listening to port ' + process.env.PORT);
 
 //Our connection to the DB
-db = mysql.createConnection({
+var pool = mysql.createPool({
+  connectionLimit : 10,
   host: 'eu-cdbr-west-01.cleardb.com',
   user: 'b18bd6b4e35f99',
   password: 'd317e611',
   database: 'heroku_ba0a838a03c77b3'
 });
 
-//We check if the connection was successful
-db.connect((err) => {
-  if (err) { throw err; }
-  console.log("DB connection OK");
-});
-
-db.on('error', function(err) {
-  console.log(err.code);
-  console.log("Attempting to create a new connection");
-  db = mysql.createConnection({
-    host: 'eu-cdbr-west-01.cleardb.com',
-    user: 'b18bd6b4e35f99',
-    password: 'd317e611',
-    database: 'heroku_ba0a838a03c77b3'
-  });
-  db.connect((err) => {
-    if (err) { throw err; }
-    console.log("DB connection OK");
-  });
-});
+// db = mysql.createConnection({
+//   host: 'eu-cdbr-west-01.cleardb.com',
+//   user: 'b18bd6b4e35f99',
+//   password: 'd317e611',
+//   database: 'heroku_ba0a838a03c77b3'
+// });
+//
+// //We check if the connection was successful
+// db.connect((err) => {
+//   if (err) { throw err; }
+//   console.log("DB connection OK");
+// });
+//
+// db.on('error', function(err) {
+//   console.log(err.code);
+//   console.log("Attempting to create a new connection");
+//   db = mysql.createConnection({
+//     host: 'eu-cdbr-west-01.cleardb.com',
+//     user: 'b18bd6b4e35f99',
+//     password: 'd317e611',
+//     database: 'heroku_ba0a838a03c77b3'
+//   });
+//   db.connect((err) => {
+//     if (err) { throw err; }
+//     console.log("DB connection OK");
+//   });
+// });
 
 app.get('/createQuestion', function(req, res){
   res.sendFile(__dirname + '/PreparationQuestion.html');
@@ -56,59 +64,67 @@ app.post('/createQuestion', function(req, res){
   //On regarde si l'UE est déjà dans la liste des UE qui existe, si non on la créer.
   var sqlVerifUE = `SELECT libelle FROM categorie WHERE libelle = '${req.body.ue}'`;
 
-  //Execute la querry Select
-  db.query(sqlVerifUE, function (err, resultsVerifUE) {
-    if(err) { throw err; }
-    console.log("Number of affected rows: " + resultsVerifUE.length);
+  //We acquire a connection from the pool
+  pool.getConnection(function(err, db) {
+  if (err) throw err; // not connected!
 
-    var okToLaunchInsertQuestion = false;
+    //Execute la querry Select
+    db.query(sqlVerifUE, function (err, resultsVerifUE) {
+      if(err) { throw err; }
+      console.log("Number of affected rows: " + resultsVerifUE.length);
 
-    if(resultsVerifUE.length >= 2)
-    {
-      console.log("On a 2 fois le même UE dans la table, contactez le DB Admin");
-      console.log(resultsVerifUE);
-      res.end('ERROR');
-    }
+      var okToLaunchInsertQuestion = false;
 
-    if(resultsVerifUE.length == 1)
-    {
-      okToLaunchInsertQuestion = true;
-    }
+      if(resultsVerifUE.length >= 2)
+      {
+        console.log("On a 2 fois le même UE dans la table, contactez le DB Admin");
+        console.log(resultsVerifUE);
+        res.end('ERROR');
+      }
 
-    if(resultsVerifUE.length == 0)
-    {
-      //On créer une nouvelle categorie
-      console.log("On va créer une nouvelle UE !");
+      if(resultsVerifUE.length == 1)
+      {
+        okToLaunchInsertQuestion = true;
+      }
 
-      // Ideally we'd put that *after* the query but asynchronicity is a mean beast
-      // And using a promise here would force me to duplicate some code into "if(resultsVerifUE.length == 1)"
-      // At least I think and I don't have enough time to polish that right now
-      okToLaunchInsertQuestion = true;
+      if(resultsVerifUE.length == 0)
+      {
+        //On créer une nouvelle categorie
+        console.log("On va créer une nouvelle UE !");
 
-      var sqlNewUE = `INSERT INTO categorie (libelle) VALUES ('${req.body.ue}')`;
-      db.query(sqlNewUE, function (err, resultsNewUE) {
-        if(err) { throw err; }
-        console.log("New UE added !");
-      });
-    }
+        // Ideally we'd put that *after* the query but asynchronicity is a mean beast
+        // And using a promise here would force me to duplicate some code into "if(resultsVerifUE.length == 1)"
+        // At least I think and I don't have enough time to polish that right now
+        okToLaunchInsertQuestion = true;
 
-    console.log("Is it ok ? <" + okToLaunchInsertQuestion + ">");
-    if(okToLaunchInsertQuestion == true)
-    {
-      console.log("On insert la question");
+        var sqlNewUE = `INSERT INTO categorie (libelle) VALUES ('${req.body.ue}')`;
+        db.query(sqlNewUE, function (err, resultsNewUE) {
+          if(err) { throw err; }
+          console.log("New UE added !");
+        });
+      }
 
-      //Changer categorie en categorie quand on passe en production
-      var sql = `INSERT INTO questionnaire (question, reponse, fausseReponse1, fausseReponse2, fausseReponse3, difficulty, ID_categorie)
-                                    VALUES ('${req.body.question}', '${req.body.proposition1}', '${req.body.proposition2}', '${req.body.proposition3}',
-                                            '${req.body.proposition4}', '${req.body.difficultee}',
-                                            (SELECT ID_categorie FROM categorie WHERE libelle = '${req.body.ue}'))`;
+      console.log("Is it ok ? <" + okToLaunchInsertQuestion + ">");
+      if(okToLaunchInsertQuestion == true)
+      {
+        console.log("On insert la question");
 
-      db.query(sql, function (err, results) {
-        if(err) { throw err; }
-        //console.log(results);
-      });
-    }
-  });
+        //Changer categorie en categorie quand on passe en production
+        var sql = `INSERT INTO questionnaire (question, reponse, fausseReponse1, fausseReponse2, fausseReponse3, difficulty, ID_categorie)
+                                      VALUES ('${req.body.question}', '${req.body.proposition1}', '${req.body.proposition2}', '${req.body.proposition3}',
+                                              '${req.body.proposition4}', '${req.body.difficultee}',
+                                              (SELECT ID_categorie FROM categorie WHERE libelle = '${req.body.ue}'))`;
+
+        db.query(sql, function (err, results) {
+          if(err) { throw err; }
+          //console.log(results);
+        });
+      }
+    });
+
+    db.release;
+  }); //Pool closed
+
   //Temporary
   res.end('Boop');
 });
@@ -128,29 +144,36 @@ app.post('/signUpBeta', function(req, res){
   //We setup the query to insert the user's credentials into profil
   var sql = `INSERT INTO profil (login, password) VALUES ('${req.body.email}', '${req.body.pswrd}')`;
 
-  //We execute the query
-  db.query(sql, function (err, results) {
-    if(err) { throw err; }
-    //console.log(results);
-  });
+  //We acquire a connection from the pool
+  pool.getConnection(function(err, db) {
+  if (err) throw err; // not connected!
 
-  //We setup the query to insert the user's info into users
-  var sql = `INSERT INTO users (nom, prenom, email, password, ID_profil) VALUES
-                        ('${req.body.nom}', '${req.body.prenom}', '${req.body.email}', '${req.body.pswrd}',
-                        (SELECT ID_Profil FROM profil WHERE login LIKE '${req.body.email}'))`;
+    //We execute the query
+    db.query(sql, function (err, results) {
+      if(err) { throw err; }
+      //console.log(results);
+    });
 
-  //We execute the query
-  db.query(sql, function (err, results) {
-    if(err) { throw err; }
-    //console.log(results);
-  });
+    //We setup the query to insert the user's info into users
+    var sql = `INSERT INTO users (nom, prenom, email, password, ID_profil) VALUES
+                          ('${req.body.nom}', '${req.body.prenom}', '${req.body.email}', '${req.body.pswrd}',
+                          (SELECT ID_Profil FROM profil WHERE login LIKE '${req.body.email}'))`;
+
+    // We execute the query
+    db.query(sql, function (err, results) {
+      if(err) { throw err; }
+      // console.log(results);
+    });
 
 
-  //On fait notre query
-  db.query("SELECT * FROM `users`", function (err, results) {
-    if (err) { throw err; }
-    console.log(results);
-  });
+    // On fait notre query
+    db.query("SELECT * FROM `users`", function (err, results) {
+      if (err) { throw err; }
+      console.log(results);
+    });
+
+    db.release();
+  }); // pool closed
 
   //Temporary
   res.end('Boop');
