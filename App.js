e701d598d2a4094a5ac5759b00be810ee8802701
Lucas Lebret <lucaslebret.scolaire@gmail.com>
2021-05-10 19:22:46 +0200

    Here is to hoping it didn't blow up

--- App.js
+++ App.js
@@ -14,17 +14,35 @@ app.use(express.static('./public'));
 //urlencoder will parse our POST data
 app.use(express.urlencoded({extended: true}));
 
+app.set('port', process.env.PORT || 3000);
+
 //We're on port 3000
-app.listen(process.env.PORT);
-console.log('Listening to port ' + process.env.PORT);
+app.listen(app.get('port'));
+console.log('Listening to port ' + app.get('port'));
 
 //Our connection to the DB
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host: 'eu-cdbr-west-01.cleardb.com',
-  user: 'b18bd6b4e35f99',
-  password: 'd317e611',
-  database: 'heroku_ba0a838a03c77b3'
+if(app.get('port') == 3000) //Local w/ Node App or Nodemon App
+{
+  var pool = mysql.createPool({
+    connectionLimit : 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'user',
+    database: 'mydb'
+  });
+
+} else // Heroku
+{
+  var pool = mysql.createPool({
+    connectionLimit : 10,
+    host: 'eu-cdbr-west-01.cleardb.com',
+    user: 'b18bd6b4e35f99',
+    password: 'd317e611',
+    database: 'heroku_ba0a838a03c77b3'
+  });
+}
+app.get('/createQuestion', function(req, res){
+  res.sendFile(__dirname + '/PreparationQuestion.html');
 });
 
 app.get('/login', function(req, res){
@@ -43,7 +61,9 @@ app.post('/login', function(req, res){
 
     // !!! They are ` and not ' !!! (alt gr + 7)
     //We setup the query to insert the user's credentials into profil
-    var sql = `SELECT login FROM profil WHERE login LIKE '${req.body.email}' AND password LIKE '${req.body.pswrd}'`;
+    var sql = `SELECT login FROM profil WHERE login LIKE ? AND password LIKE ?`;
+    var inserts = [req.body.email, req.body.pswrd];
+    sql = mysql.format(sql, inserts);
 
     //We execute the query
     db.query(sql, function (err, results) {
@@ -61,6 +81,8 @@ app.post('/login', function(req, res){
       }
       else {
         console.log("More than one account with the same credentials ?");
+        console.log(results.length);
+        console.log(results);
       }
     });
 
@@ -72,16 +94,13 @@ app.post('/login', function(req, res){
   res.end('Boop');
 });
 
-
-app.get('/createQuestion', function(req, res){
-  res.sendFile(__dirname + '/PreparationQuestion.html');
-});
-
 app.post('/createQuestion', function(req, res){
 
   console.log("In createQuestion");
   //On regarde si l'UE est déjà dans la liste des UE qui existe, si non on la créer.
-  var sqlVerifUE = `SELECT libelle FROM categorie WHERE libelle = '${req.body.ue}'`;
+  var sqlVerifUE = `SELECT libelle FROM cathegorie WHERE libelle = ?`;
+  var insertsVerifUE = [req.body.ue];
+  sqlVerifUE = mysql.format(sqlVerifUE, insertsVerifUE);
 
   //We acquire a connection from the pool
   pool.getConnection(function(err, db) {
@@ -108,7 +127,7 @@ app.post('/createQuestion', function(req, res){
 
       if(resultsVerifUE.length == 0)
       {
-        //On créer une nouvelle categorie
+        //On créer une nouvelle cathegorie
         console.log("On va créer une nouvelle UE !");
 
         // Ideally we'd put that *after* the query but asynchronicity is a mean beast
@@ -116,7 +135,10 @@ app.post('/createQuestion', function(req, res){
         // At least I think and I don't have enough time to polish that right now
         okToLaunchInsertQuestion = true;
 
-        var sqlNewUE = `INSERT INTO categorie (libelle) VALUES ('${req.body.ue}')`;
+        var sqlNewUE = `INSERT INTO cathegorie SET ?`;
+        var insertNewUE = {libelle: req.body.ue};
+        sqlNewUE = mysql.format(sqlNewUE, insertNewUE);
+
         db.query(sqlNewUE, function (err, resultsNewUE) {
           if(err) { throw err; }
           console.log("New UE added !");
@@ -128,18 +150,25 @@ app.post('/createQuestion', function(req, res){
       {
         console.log("On insert la question");
 
-        //Changer categorie en categorie quand on passe en production
-        var sql = `INSERT INTO questionnaire (question, reponse, fausseReponse1, fausseReponse2, fausseReponse3, difficulty, ID_categorie)
-                                      VALUES ('${req.body.question}', '${req.body.proposition1}', '${req.body.proposition2}', '${req.body.proposition3}',
-                                              '${req.body.proposition4}', '${req.body.difficultee}',
-                                              (SELECT ID_categorie FROM categorie WHERE libelle = '${req.body.ue}'))`;
+        var sqlSelectLibelle = `SELECT ID_cathegorie FROM cathegorie WHERE libelle = ?`;
+        sqlSelectLibelle = mysql.format(sqlSelectLibelle,  [req.body.ue]);
 
-        db.query(sql, function (err, results) {
-          if(err) { throw err; }
-          //console.log(results);
-        });
-      }
-    });
+        db.query(sqlSelectLibelle, function (err, resultsLibelle)
+        {
+          var sql = `INSERT INTO questionnaire SET ?`;
+          var inserts =  {question: req.body.question, reponse: req.body.proposition1,
+                          fausseReponse1: req.body.proposition2, fausseReponse2: req.body.proposition3,
+                          fausseReponse3: req.body.proposition4, difficulty: req.body.difficultee,
+                          ID_cathegorie: resultsLibelle[0].ID_cathegorie};
+          sql = mysql.format(sql, inserts);
+
+          db.query(sql, function (err, results) {
+            if(err) { throw err; }
+            console.log(results);
+          });// Insert Into Questionnaire
+        });// Select Libelle
+      }// If it's ok to insert la question
+    });// Verif UE
 
     db.release;
   }); //Pool closed
@@ -165,33 +194,41 @@ app.post('/signUpBeta', function(req, res){
 
     // !!! They are ` and not ' !!! (alt gr + 7)
     //We setup the query to insert the user's credentials into profil
-    var sql = `INSERT INTO profil (login, password) VALUES ('${req.body.email}', '${req.body.pswrd}')`;
+    var sql = `INSERT INTO profil SET ?`;
+    var inserts = {login: req.body.email, password: req.body.pswrd}
+    sql = mysql.format(sql, inserts);
+    console.log(sql);
 
     //We execute the query
     db.query(sql, function (err, results) {
       if(err) { throw err; }
-      //console.log(results);
-    });
 
-    console.log("Profile Insert OK");
-    //We setup the query to insert the user's info into users
-    var sql = `INSERT INTO users (nom, prenom, email, password, ID_profil) VALUES
-                          ('${req.body.nom}', '${req.body.prenom}', '${req.body.email}', '${req.body.pswrd}',
-                          (SELECT ID_Profil FROM profil WHERE login LIKE '${req.body.email}'))`;
+      console.log("Profile Insert OK");
+      //We setup the query to insert the user's info into users
 
-    // We execute the query
-    db.query(sql, function (err, results) {
-      if(err) { throw err; }
-      // console.log(results);
-    });
+      var sqlSelectIDPro = `SELECT ID_Profil FROM profil WHERE login LIKE ?`;
+      var insertIDPro = [req.body.email];
+      sqlSelectIDPro = mysql.format(sqlSelectIDPro, insertIDPro);
 
+      db.query(sqlSelectIDPro, function (err, resultsIDPro) {
 
-    // On fait notre query
-    console.log("Select Query");
-    db.query("SELECT * FROM `users`", function (err, results) {
-      if (err) { throw err; }
-      console.log(results);
-    });
+        var sql = `INSERT INTO users SET ?`;
+        var inserts = {nom: req.body.nom, prenom: req.body.prenom, email: req.body.email,
+                       password: req.body.pswrd, ID_profil: resultsIDPro[0].ID_Profil};
+        sql = mysql.format(sql, inserts);
+
+        // We execute the query
+        db.query(sql, function (err, results) {
+          if(err) { throw err; }
+          // On fait notre query
+            console.log("Select Query");
+            db.query("SELECT * FROM `users`", function (err, results) {
+              if (err) { throw err; }
+              console.log(results);
+          });// Select Querry
+        });// Insert Into Users Querry
+      });// Select ID_Profil Querry
+    });// Insert Profil
 
     db.release();
   }); // pool closed
